a920826894eff47ae4bbbeca265d82115e340550
Noel Xavier Ulpindo <noel.ulpindo@gmail.com>
2022-09-20 09:42:08 +0800

    updated template, added inertia progress, updated app.js for entry routes, updated readme

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,32 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
-  resolve: name => import(`./Pages/${name}.vue`),
+  resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .component('Link', Link)
+      .component('Head', Head)
       .mount(el)
   },
+  // The ${title} will be displayed in the head tags on your pages
+  title: title => `YOUR TITLE | ${title}`
+})
+
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 250,
+
+  // The color of the progress bar.
+  color: '#29d',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: false,
 })
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Login');
+    return Inertia::render('Welcome');
 });
